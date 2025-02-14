1c9ec4a702311266122b61aa1b3e99512ec6cd81
Esteban <esteban.castro@contentolatam.com>
2025-02-14 16:26:38 -0500

    verify Login

--- src/Routes/testRoute.js
+++ src/Routes/testRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const pool = require('../db'); 
 const router = express.Router();
 const {getUser} = require('../controllers/register.controller');
+const {VerifyLogin}=require('../controllers/login.controller')
 
 router.get('/', (req, res) => {
     res.send('API is working!');
 });
 
 router.post('/addRegister',getUser);
+router.post('/Login',VerifyLogin);
 
 module.exports = router;
--- src/db.js
+++ src/db.js
@@ -11,4 +11,5 @@ const pool = new Pool({
     database: process.env.DB_DATABASE
 });
 
+
 module.exports = pool;
