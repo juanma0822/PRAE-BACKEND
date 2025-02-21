278998eda8ea735ae80b87ac2f494a09d76fd11d
juanma0822 <juanmanuelva3243@gmail.com>
2025-02-20 22:31:31 -0500

    ``` No code changes made. ```

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ app.use('/calificacion', calificacionRoutes);
 //TEST API
 app.use('/test', testRoute);
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
