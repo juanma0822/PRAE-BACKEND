ef05c1b0be806e80b2beffb94eab81b974cd742d
juanma0822 <juanmanuelva3243@gmail.com>
2025-02-20 22:34:07 -0500

    Corrección de puerto para Vercel

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ app.use('/calificacion', calificacionRoutes);
 //TEST API
 app.use('/test', testRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT || 'default'}`);
 });
