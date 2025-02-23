5c9460b6fc64283d8c13ccefcad3e4d3e51a642a
Esteban <esteban.castro@contentolatam.com>
2025-02-22 22:41:27 -0500

    new changes

--- src/Routes/rutascontroller.js
+++ src/Routes/rutascontroller.js
@@ -1,4 +1,4 @@
-const{recoverPassword,validateResetToken } =require ("../controllers/recoverPssword.controller.js")
+const{recoverPassword, validateResetToken } =require ("../controllers/recoverPssword.controller.js")
 const {VerifyLogin} =require ("../controllers/login.controller.js")
 const { sendMessage,getChatHistory}=require("../controllers/chatbot.js")
 const {generatePdf}=require("../controllers/generatePdf.controller.js")
