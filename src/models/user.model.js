99a2030cd49001fff93f7409b11a56948ab89734
Esteban <esteban.castro@contentolatam.com>
2025-02-24 14:12:38 -0500

    new routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -35,7 +35,7 @@ const getMessages = async (sender_id, receiver_id) => {
        ORDER BY created_at ASC`, 
       [sender_id, receiver_id]
     );
-  
+
     return result.rows; 
   };
 
@@ -52,7 +52,7 @@ const getMessages = async (sender_id, receiver_id) => {
         WHERE curso.nombre = $1 AND materia.nombre = $2`;
       
       let values = [curse, subject];
-  
+      console.log(values)
       if (id !="all") {
         query += " AND estudiante.documento_identidad = $3";
         values.push(id);
@@ -67,12 +67,31 @@ const getMessages = async (sender_id, receiver_id) => {
     }
   };
   
-const FindOneStudentModels=async()=>{
-try{
-
-}catch(e){
-  return e
-}
-}
-
+  const FindOneStudentModels = async (student_id) => {
+    try {
+      let query = `
+        SELECT m.nombre AS materia, a.nombre AS actividad, peso, nota
+        FROM estudiante e
+        JOIN calificacion c ON e.documento_identidad = c.id_estudiante  
+        JOIN actividades a ON c.id_actividad = a.id_actividad
+        JOIN materia m ON a.id_materia = m.id_materia
+      `;
+  
+      let values = [];
+  
+      if (student_id !== "all") {
+        query += " WHERE c.id_estudiante = $1";  
+        values.push(student_id);
+      }
+  
+   
+  
+      const result = await Pool.query(query, values);
+      return result.rows;
+    } catch (e) {
+      console.error("Error en FindOneStudentModels:", e);
+      throw e;
+    }
+  };
+  
 module.exports = {ExistingUser,saveMessage,getMessages,SearchDataAllStudents,FindOneStudentModels}
