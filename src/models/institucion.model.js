3d4190f85485d7a91eee109a41cc1d678c360290
juanma0822 <juanmanuelva3243@gmail.com>
2025-03-14 12:21:58 -0500

    Set default values for color fields in updateInstitucion function

--- src/models/institucion.model.js
+++ src/models/institucion.model.js
@@ -42,10 +42,18 @@ const getInstitucionById = async (id_institucion) => {
 
 // Actualizar una institución
 const updateInstitucion = async (id_institucion, nombre, telefono, instagram, facebook, direccion, logo, color_principal, color_secundario, fondo, color_pildora1, color_pildora2, color_pildora3) => {
+  // Asignar valores por defecto si no se proporcionan
+  color_principal = color_principal || '#157AFE';
+  color_secundario = color_secundario || '#F5F7F9';
+  fondo = fondo || '#FFFFFF';
+  color_pildora1 = color_pildora1 || '#157AFE';
+  color_pildora2 = color_pildora2 || '#4946E2';
+  color_pildora3 = color_pildora3 || '#EF9131';
+  
   const query = `
     UPDATE Institucion
-    SET nombre = $1, telefono = $2, instagram = $3, facebook = $4, direccion= $5 logo = $6, color_principal = $6, color_secundario = $7, fondo = $8, color_pildora1 = $9, color_pildora2 = $10, color_pildora3 = $11
-    WHERE id_institucion = $12 AND estado = TRUE
+    SET nombre = $1, telefono = $2, instagram = $3, facebook = $4, direccion= $5, logo = $6, color_principal = $7, color_secundario = $8, fondo = $9, color_pildora1 = $10, color_pildora2 = $11, color_pildora3 = $12
+    WHERE id_institucion = $13 AND estado = TRUE
     RETURNING *;
   `;
   const values = [nombre, telefono, instagram, facebook, direccion, logo, color_principal, color_secundario, fondo, color_pildora1, color_pildora2, color_pildora3, id_institucion];
