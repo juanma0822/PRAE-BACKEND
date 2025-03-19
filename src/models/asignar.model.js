de7941efdcbd0b7747aee851915a159c04d33060
juanma0822 <juanmanuelva3243@gmail.com>
2025-03-19 12:07:53 -0500

    Update model of obtenerAsignacionesPorProfesor

--- src/models/asignar.model.js
+++ src/models/asignar.model.js
@@ -131,23 +131,22 @@ const obtenerGradosPorProfesor = async (documento_profe) => {
 
 const obtenerAsignacionesPorProfesor = async (documento_profe, id_institucion) => {
   const query = `
-    SELECT DISTINCT 
+    SELECT 
       a.id_asignacion,
-      c.id_curso,
+      c.id_curso, 
       c.nombre AS curso,
       m.id_materia,
-      m.nombre AS materia,
-      m.color,
-      u.documento_identidad AS profesor_documento,
-      u.nombre AS profesor_nombre,
+      m.color, 
+      m.nombre AS materia, 
+      u.documento_identidad AS profesor_documento, 
+      u.nombre AS profesor_nombre, 
       u.apellido AS profesor_apellido
-    FROM Dictar d
-    INNER JOIN Materia m ON d.id_materia = m.id_materia
-    INNER JOIN Asignar a ON m.id_materia = a.id_materia
+    FROM Asignar a
     INNER JOIN Curso c ON a.id_curso = c.id_curso
-    INNER JOIN Profesor p ON d.documento_profe = p.documento_identidad
-    INNER JOIN Usuario u ON p.documento_identidad = u.documento_identidad
-    WHERE a.id_docente = $1 AND c.id_institucion = $2 AND a.estado = TRUE;
+    INNER JOIN Materia m ON a.id_materia = m.id_materia
+    LEFT JOIN Profesor p ON a.id_docente = p.documento_identidad
+    LEFT JOIN Usuario u ON p.documento_identidad = u.documento_identidad
+    WHERE a.id_docente= $1 AND c.id_institucion = $2 AND a.estado = TRUE;
   `;
   const result = await consultarDB(query, [documento_profe, id_institucion]);
   return result;
