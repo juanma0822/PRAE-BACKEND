223cee53dfd09f3dee15434661dc744f74d6e2f6
juanma0822 <juanmanuelva3243@gmail.com>
2025-04-16 21:03:46 -0500

    Refactor selectCalificacionesEstudiante for improved query efficiency and enhanced readability

--- src/models/calificacion.model.js
+++ src/models/calificacion.model.js
@@ -1,68 +1,90 @@
-const { consultarDB } = require('../db');
+const { consultarDB } = require("../db");
 
 const insertCalificacion = async (id_actividad, id_estudiante, nota) => {
-    const query = `
+  const query = `
         INSERT INTO Calificacion (id_actividad, id_estudiante, nota) 
         VALUES ($1, $2, $3) RETURNING *`;
-    const values = [id_actividad, id_estudiante, nota];
-    const result = await consultarDB(query, values);
-    return result[0];
+  const values = [id_actividad, id_estudiante, nota];
+  const result = await consultarDB(query, values);
+  return result[0];
 };
 
 const updateCalificacion = async (id_calificacion, nota) => {
-    const query = `
+  const query = `
         UPDATE Calificacion 
         SET nota = $1 
         WHERE id_calificacion = $2 RETURNING *`;
-    const values = [nota, id_calificacion];
-    const result = await consultarDB(query, values);
-    return result[0];
+  const values = [nota, id_calificacion];
+  const result = await consultarDB(query, values);
+  return result[0];
 };
 
 const selectCalificacionesEstudiante = async (id_materia, id_estudiante) => {
-    const query = `
+  const query = `
         SELECT c.id_calificacion, c.nota, a.nombre AS actividad, c.id_actividad
         FROM Calificacion c
         JOIN Actividades a ON c.id_actividad = a.id_actividad
         WHERE a.id_materia = $1 AND c.id_estudiante = $2`;
-    const result = await consultarDB(query, [id_materia, id_estudiante]);
-    return result;
+  const result = await consultarDB(query, [id_materia, id_estudiante]);
+  return result;
 };
 
-const selectCalificacionesEstudiantePorDocenteEInstitucion = async (id_materia, id_estudiante, id_docente, id_institucion) => {
-    const query = `
-        SELECT 
-            a.id_actividad,
-            a.nombre AS actividad,
-            a.peso,
-            COALESCE(c.nota, 0) AS nota,
-            c.id_calificacion
-        FROM Actividades a
-        LEFT JOIN Calificacion c ON a.id_actividad = c.id_actividad AND c.id_estudiante = $2
-        JOIN Materia m ON a.id_materia = m.id_materia
-        WHERE a.id_materia = $1 
-          AND a.id_docente = $3 
-          AND m.id_institucion = $4
-          AND a.activo = TRUE
-          AND c.activo = TRUE;
+const selectCalificacionesEstudiantePorDocenteEInstitucion = async (
+  id_materia,
+  id_estudiante,
+  id_docente,
+  id_institucion
+) => {
+  const query = `
+      SELECT 
+        a.id_actividad,
+        a.nombre AS actividad,
+        a.peso,
+        COALESCE(c.nota, 0) AS nota,
+        c.id_calificacion
+      FROM Actividades a
+      LEFT JOIN Calificacion c 
+        ON a.id_actividad = c.id_actividad 
+       AND c.id_estudiante = $2 
+       AND c.activo = TRUE
+      JOIN Materia m 
+        ON a.id_materia = m.id_materia
+      WHERE a.id_materia = $1 
+        AND a.id_docente = $3 
+        AND m.id_institucion = $4
+        AND a.activo = TRUE
+      ORDER BY a.nombre ASC;
     `;
-    const result = await consultarDB(query, [id_materia, id_estudiante, id_docente, id_institucion]);
-    return result;
+  return consultarDB(query, [
+    id_materia,
+    id_estudiante,
+    id_docente,
+    id_institucion,
+  ]);
 };
 
-const selectCalificacionesCurso = async (id_materia, id_curso, id_docente, id_institucion) => {
-    // Primero, comprobamos si hay actividades para esa materia y curso
-    const checkActivitiesQuery = `
+const selectCalificacionesCurso = async (
+  id_materia,
+  id_curso,
+  id_docente,
+  id_institucion
+) => {
+  // Primero, comprobamos si hay actividades para esa materia y curso
+  const checkActivitiesQuery = `
         SELECT COUNT(*) 
         FROM Actividades a
         WHERE a.id_materia = $1 AND a.id_curso = $2 AND a.id_docente = $3 AND a.activo = TRUE;
     `;
-    
-    const activityCount = await consultarDB(checkActivitiesQuery, [id_materia, id_curso, id_docente]);
 
-    // Si hay actividades, realizamos la consulta de calificaciones con actividades
-    if (activityCount[0].count > 0) {
-        const queryWithActivities = `
+  const activityCount = await consultarDB(checkActivitiesQuery, [
+    id_materia,
+    id_curso,
+    id_docente,
+  ]);
+
+  // Si hay actividades, realizamos la consulta de calificaciones con actividades
+  if (activityCount[0].count > 0) {
+    const queryWithActivities = `
             SELECT 
                 u.documento_identidad, 
                 u.nombre, 
@@ -83,38 +105,67 @@ const selectCalificacionesCurso = async (id_materia, id_curso, id_docente, id_in
               AND u.activo = TRUE 
             ORDER BY u.nombre ASC;
         `;
-        
-        const result = await consultarDB(queryWithActivities, [id_materia, id_curso, id_docente, id_institucion]);
 
-        // Agrupar los resultados: Aseguramos que siempre haya un listado de estudiantes
-        const groupedResult = result.reduce((acc, row) => {
-            const { documento_identidad, nombre, apellido, nota, id_calificacion, actividad, peso, id_actividad, id_curso } = row;
-            
-            if (!acc[documento_identidad]) {
-                acc[documento_identidad] = {
-                    documento_identidad,
-                    nombre,
-                    apellido,
-                    id_curso,  // Añadir id_curso aquí
-                    actividades: []
-                };
-            }
+    const result = await consultarDB(queryWithActivities, [
+      id_materia,
+      id_curso,
+      id_docente,
+      id_institucion,
+    ]);
 
-            // Solo añadimos actividades si existen
-            if (actividad) {
-                acc[documento_identidad].actividades.push({ id_actividad, actividad, peso, nota, id_calificacion });
-            } else {
-                // Si no hay actividad, mostramos "Sin actividad asignada"
-                acc[documento_identidad].actividades.push({ id_actividad: null, actividad: "Sin actividad asignada", peso: 0, nota, id_calificacion: null });
-            }
+    // Agrupar los resultados: Aseguramos que siempre haya un listado de estudiantes
+    const groupedResult = result.reduce((acc, row) => {
+      const {
+        documento_identidad,
+        nombre,
+        apellido,
+        nota,
+        id_calificacion,
+        actividad,
+        peso,
+        id_actividad,
+        id_curso,
+      } = row;
 
-            return acc;
-        }, {});
+      if (!acc[documento_identidad]) {
+        acc[documento_identidad] = {
+          documento_identidad,
+          nombre,
+          apellido,
+          id_curso, // Añadir id_curso aquí
+          actividades: [],
+        };
+      }
 
-        return Object.values(groupedResult).sort((a, b) => a.apellido.localeCompare(b.apellido));
-    } else {
-        // Si no hay actividades, simplemente traemos la lista de estudiantes
-        const queryOnlyStudents = `
+      // Solo añadimos actividades si existen
+      if (actividad) {
+        acc[documento_identidad].actividades.push({
+          id_actividad,
+          actividad,
+          peso,
+          nota,
+          id_calificacion,
+        });
+      } else {
+        // Si no hay actividad, mostramos "Sin actividad asignada"
+        acc[documento_identidad].actividades.push({
+          id_actividad: null,
+          actividad: "Sin actividad asignada",
+          peso: 0,
+          nota,
+          id_calificacion: null,
+        });
+      }
+
+      return acc;
+    }, {});
+
+    return Object.values(groupedResult).sort((a, b) =>
+      a.apellido.localeCompare(b.apellido)
+    );
+  } else {
+    // Si no hay actividades, simplemente traemos la lista de estudiantes
+    const queryOnlyStudents = `
             SELECT 
                 u.documento_identidad, 
                 u.nombre, 
@@ -126,23 +177,26 @@ const selectCalificacionesCurso = async (id_materia, id_curso, id_docente, id_in
               AND u.activo = TRUE 
             ORDER BY u.nombre ASC;
         `;
-        
-        const studentsResult = await consultarDB(queryOnlyStudents, [id_curso]);
 
-        // Agrupar solo estudiantes sin actividades
-        return studentsResult.map((student) => ({
-            documento_identidad: student.documento_identidad,
-            nombre: student.nombre,
-            apellido: student.apellido,
-            id_curso: student.id_curso,
-            actividades: []  // No hay actividades
-        }));
-    }
+    const studentsResult = await consultarDB(queryOnlyStudents, [id_curso]);
+
+    // Agrupar solo estudiantes sin actividades
+    return studentsResult.map((student) => ({
+      documento_identidad: student.documento_identidad,
+      nombre: student.nombre,
+      apellido: student.apellido,
+      id_curso: student.id_curso,
+      actividades: [], // No hay actividades
+    }));
+  }
 };
 
-
-const selectPromedioEstudiante = async (id_materia, id_estudiante, id_docente) => {
-    const query = `
+const selectPromedioEstudiante = async (
+  id_materia,
+  id_estudiante,
+  id_docente
+) => {
+  const query = `
         SELECT 
             CASE 
                 WHEN SUM(a.peso) > 0 THEN SUM(c.nota * (a.peso / 100.0)) / SUM(a.peso / 100.0)
@@ -154,12 +208,16 @@ const selectPromedioEstudiante = async (id_materia, id_estudiante, id_docente) =
           AND c.id_estudiante = $2
           AND a.id_docente = $3;
     `;
-    const result = await consultarDB(query, [id_materia, id_estudiante, id_docente]);
-    return result[0]?.promedio || 0; // Retorna 0 si no hay calificaciones
+  const result = await consultarDB(query, [
+    id_materia,
+    id_estudiante,
+    id_docente,
+  ]);
+  return result[0]?.promedio || 0; // Retorna 0 si no hay calificaciones
 };
 
 const selectPromedioCursoMateria = async (id_materia, id_curso) => {
-    const query = `
+  const query = `
       SELECT 
         u.nombre,
         u.apellido,
@@ -174,43 +232,49 @@ const selectPromedioCursoMateria = async (id_materia, id_curso) => {
       WHERE e.id_curso = $2
       GROUP BY u.nombre, u.apellido;
     `;
-  
-    const rows = await consultarDB(query, [id_materia, id_curso]);
-  
-    // Calcular promedio general del curso
-    const promedioCurso = rows.length > 0
-      ? parseFloat((rows.reduce((acc, r) => acc + parseFloat(r.promedio), 0) / rows.length).toFixed(2))
+
+  const rows = await consultarDB(query, [id_materia, id_curso]);
+
+  // Calcular promedio general del curso
+  const promedioCurso =
+    rows.length > 0
+      ? parseFloat(
+          (
+            rows.reduce((acc, r) => acc + parseFloat(r.promedio), 0) /
+            rows.length
+          ).toFixed(2)
+        )
       : 0;
-  
-    // Construir respuesta como JSON personalizado
-    const resultado = {
-      promedioCurso,
-    };
-  
-    rows.forEach((row) => {
-      const key = `${row.nombre} ${row.apellido}`;
-      resultado[key] = parseFloat(row.promedio);
-    });
-  
-    return resultado;
+
+  // Construir respuesta como JSON personalizado
+  const resultado = {
+    promedioCurso,
+  };
+
+  rows.forEach((row) => {
+    const key = `${row.nombre} ${row.apellido}`;
+    resultado[key] = parseFloat(row.promedio);
+  });
+
+  return resultado;
 };
 
 const getCalificacionById = async (id_calificacion) => {
-    const query = `
+  const query = `
       SELECT * FROM Calificacion
       WHERE id_calificacion = $1;
     `;
-    const result = await consultarDB(query, [id_calificacion]);
-    return result[0];
+  const result = await consultarDB(query, [id_calificacion]);
+  return result[0];
 };
 
 module.exports = {
-    insertCalificacion,
-    updateCalificacion,
-    selectCalificacionesEstudiante,
-    selectCalificacionesEstudiantePorDocenteEInstitucion,
-    selectCalificacionesCurso,
-    selectPromedioEstudiante,
-    selectPromedioCursoMateria,
-    getCalificacionById,
+  insertCalificacion,
+  updateCalificacion,
+  selectCalificacionesEstudiante,
+  selectCalificacionesEstudiantePorDocenteEInstitucion,
+  selectCalificacionesCurso,
+  selectPromedioEstudiante,
+  selectPromedioCursoMateria,
+  getCalificacionById,
 };
